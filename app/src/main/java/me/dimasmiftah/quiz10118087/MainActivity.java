814bd485882f6e2591601468df27ef774c2f0db5
Dimas Miftahul Huda <dimas.mfth@gmail.com>
2021-05-29 10:58:41 +0700

    add identity comments

--- app/src/main/java/me/dimasmiftah/quiz10118087/MainActivity.java
+++ app/src/main/java/me/dimasmiftah/quiz10118087/MainActivity.java
@@ -1,5 +1,10 @@
 package me.dimasmiftah.quiz10118087;
 
+//        Tanggal pengerjaan: Sabtu, 29 Mei 2021 10:40
+//        NIM: 10118087
+//        Nama: Dimas Miftahul Huda
+//        Kelas: IF-2
+
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
